c653522709687c4f9f7e56e532cbf3d6e47d56b6
Mira <Miroslavaung@gmail.com>
2025-11-15 17:10:20 +0300

    Делаем игру (3)

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 # 🌲🌊🚁🟩🔥🏥💛🥛🛒☁️⚡🏆
 
+from map import Map
+import time
+import os
+
+TICK_SLEEP = 0.0005
+TREE_UPDATE = 50
+FIRE_UPDATE = 100
+MAP_W, MAP_H = 20, 10
+
+field = Map(MAP_W,MAP_H)
+field.generate_forest(3,10)
+field.generate_river(10)
+field.generate_river(10)
+field.print_map()
+
+
+tick = 1
+
+while True:
+    os.system('cls')
+    print('TICK', tick)
+    field.print_map()
+    tick += 1
+    time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+        field.generate_tree()
+    if (tick % FIRE_UPDATE == 0):
+        field.update_fires()
+
