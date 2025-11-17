cb725362c9f1b033f0778045844dd14cfc281977
Mira <Miroslavaung@gmail.com>
2025-11-17 23:48:07 +0300

    Делаем игру (4)

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 # 🌲🌊🚁🟩🔥🏥💛🥛🛒☁️⚡🏆
 
+from pynput import keyboard
 from map import Map
 import time
 import os
+from helicopter import Helicopter as Helico
 
-TICK_SLEEP = 0.0005
+TICK_SLEEP = 0.005
 TREE_UPDATE = 50
 FIRE_UPDATE = 100
 MAP_W, MAP_H = 20, 10
 
 field = Map(MAP_W,MAP_H)
-field.generate_forest(3,10)
-field.generate_river(10)
-field.generate_river(10)
-field.print_map()
 
+
+helico = Helico(MAP_W, MAP_H)
+
+
+
+
+
+
+
+MOVES = {'w': (-1,0), 'd': (0,1), 's': (1,0), 'a': (0,-1)}
+def process_key(key):
+    global helico
+    c = key.char
+    if c in MOVES.keys():
+        dx, dy = MOVES[c][0], MOVES[c][1]
+        helico.move(dx, dy)
+
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=process_key)
+listener.start()
 
 tick = 1
 
 while True:
-    os.system('cls')
+    os.system('echo. & cls')
+    print(helico.x, helico.y)
     print('TICK', tick)
-    field.print_map()
+    field.process_helicopter(helico)
+    helico.print_stats()
+    field.print_map(helico)
     tick += 1
     time.sleep(TICK_SLEEP)
     if (tick % TREE_UPDATE == 0):
